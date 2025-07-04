b46f00760b7fbf2cae7dccd298c5d00b84176295
smeetanagannavar <nagannavarsmeeta66@gmail.com>
2025-07-04 14:34:07 +0530

    add secure password generator with character type enforcement

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 function generatePassword(){
-    const length = 12;
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
-    let password = "";
+    const length = parseInt(document.getElementById("length").value);
+
+    if(length < 4){
+        alert("length must be at least 4")
+        return ;
+    }
+
+    const lower  = "abcdefghijklmnopqrstuvwxyz";
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()_+";
+
+    const allChars = upper + lower + number + symbols;
+    let password = [
+        upper[Math.floor(Math.random()*upper.length)],
+        lower[Math.floor(Math.random()*lower.length)],
+        numbers[Math.floor(Math.random()*numbers.length)],
+        symbols[Math.floor(Math.random()*symbols.length)]
+    ]
     for(let i=0; i<length; i++){
-        const char = charset[Math.floor(Math.random()*charset.length)];
-        password += char;
+        password.push(allChars[Math.floor(Math.random()*allChars.length)]);
 
     }
-    document.getElementById("password").value = password;
+
+    password = password.sort(() => 0.5-Math.random());
+
+    document.getElementById("result").innerHTML = password.join("");
 }
